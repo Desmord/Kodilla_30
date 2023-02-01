89b74e3538352ba3129cac55a60045727dd158e6
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-02-01 15:37:58 +0100

    30.1

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,5 +1,25 @@
 module.exports = (fullName) => {
-  const [ firstName, lastName ] = fullName.split(' ');
-  if(!firstName || !lastName) return false;
-  return firstName[0].toUpperCase + firstName.slice(1).toLowerCase() + lastName[0].toUpperCase + lastName.slice(1).toLowerCase() ;
+
+  if (!fullName) return `Error`;
+  if (typeof fullName !== `string`) return `Error`;
+
+  const words = fullName.split(` `);
+
+  if (words.length <= 1 || words.length > 2) return `Error`;
+
+  const [firstName, lastName] = fullName.split(' ');
+
+  if (!firstName || !lastName) return `Error`;
+
+  let firstNameInProperFormat = words[0].toLocaleLowerCase();
+  firstNameInProperFormat =
+    firstNameInProperFormat.substring(0, 1).toUpperCase() +
+    firstNameInProperFormat.substring(1, firstNameInProperFormat.length + 1)
+
+  let lastNameInProperFormat = words[1].toLocaleLowerCase();
+  lastNameInProperFormat =
+    lastNameInProperFormat.substring(0, 1).toUpperCase() +
+    lastNameInProperFormat.substring(1, lastNameInProperFormat.length + 1)
+
+  return `${firstName} ${lastName}`
 };
